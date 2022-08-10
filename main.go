1f47141d56af5501040a5551251a2ebce809eb1f
Mauricio Rojas <mrojase@vitamina.cl>
2022-08-10 14:23:21 -0400

    chore: add basic web server and handler

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+// Define a home handler function which writes a byte slice containing
+// "Hello from Snippetbox" as the response body.
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Snippetbox"))
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	// Use the http.NewServeMux() function to initialize a new servemux, then
+	// register the home funciton as the handler for the "/" URL pattern.
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	// Use the http.ListenAndServe() function to start a new web server. We pass
+	// two parameters: the TCP network address to listen on (in this case :4000)
+	// and the servemux we just created. If http.ListenAndServe() returns an error
+	// We use log.Fatal() function to log the error message and exit.
+	log.Println("Starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
