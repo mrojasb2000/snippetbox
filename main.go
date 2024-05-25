69fabda7ba574e503920a1475c3203f266172988
Mauricio Rojas <mauricio.rojas@thoughtworks.com>
2024-05-24 22:11:38 -0400

    refactor: adding check current URL path matches with '/'

--- main.go
+++ main.go
@@ -8,7 +8,15 @@ import (
 // Define a home handler function which writes a slice containing
 // "Hello from Snippetbox" as the response body.
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from Snippetbox"))
+	// Check if the current request URL path exactly matches "/". If it does't, use
+	// the http.notFound() function to send a 404 response to the client.
+	// Importantly, we then return from handler. If we don't return the handler
+	// would keep executing and also write the "Hello from SnippetBox" message
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("Hello from SnippetBox"))
 }
 
 // Add s snippetView handler function.
