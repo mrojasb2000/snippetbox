5eca496b1439f250f603ce4e36dc1758f1f923a1
Mauricio Rojas <mauricio.rojas@thoughtworks.com>
2024-05-27 12:04:02 -0400

    refactor: adding constant and methods helpers

--- main.go
+++ main.go
@@ -26,6 +26,25 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 
 // Add a snippetCreate handler function.
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	// Use r.Method to check whether the request is using POST or not.
+	if r.Method != "POST" {
+		// Use the Header().Set() method to add an 'Allow: POST' header to the
+		// response header map. The first parameter is the header name, and
+		// the second parameter is the header value.
+		w.Header().Set("Allow", "POST")
+
+		// If it's not, use the w.WriteHeader() method to send a 405 status
+		// code and the w.Write() method to write a "Method Not Allowed"
+		// response body. We then return from the function so that the
+		// subsequent code it not executed.
+		//w.WriteHeader(405)
+		//w.Write([]byte("Method Not Allowed"))
+
+		// Use the http.Error() function to send 405 status code and "Method Not
+		// Allowed" string as the response body.
+		http.Error(w, "Method Not Allowed", 405)
+		return
+	}
 	w.Write([]byte("Create a new snippet..."))
 }
 
