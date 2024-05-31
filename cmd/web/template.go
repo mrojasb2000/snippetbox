53a49d78f7b4baf33521a532accbf4cf051b1f32
Mauricio Rojas <mauricio.rojas@thoughtworks.com>
2024-05-31 19:19:04 -0400

    refactor: adding custom function to template

--- cmd/web/template.go
+++ cmd/web/template.go
@@ -22,6 +22,19 @@ type templateData struct {
 	Snippets    []*models.Snippet
 }
 
+// Create a humanDate function which returns a nicely formatted string
+// representation of a time.Time object.
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+// Initialize a template.FuncMap object and store it in a global variable. This is
+// essentially a string-keyed map which acts as a lookup between the names of our
+// custom template functions and the functions themselves.
+var functions = template.FuncMap{
+	"humanDate": humanDate,
+}
+
 // Create an newTemplateData() helper, which returns a pointer to a templateData
 // struct initialize with the current year. Note that we're not using the
 // *http.Request parameter here at the moment, but we will do later in the book.
@@ -50,8 +63,12 @@ func newTemplateCache() (map[string]*template.Template, error) {
 		// and assign it to the name variable.
 		name := filepath.Base(page)
 
+		// The template.FunMap must be registered with the template set before you
+		// call the ParseFiles() method. This means we hace to use template.New() to
+		// create an empty template set, use the Funcs() method to register the
+		// template.FunMap, and then parse the file as normal.
 		// Parse the base template file into a template set.
-		ts, err := template.ParseFiles("./ui/html/base.tmpl")
+		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.tmpl")
 		if err != nil {
 			return nil, err
 		}
