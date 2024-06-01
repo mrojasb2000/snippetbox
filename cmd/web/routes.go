bb2b716ddea9e78bdf6d6bd284e4e6a27940f16b
Mauricio Rojas <mauricio.rojas@thoughtworks.com>
2024-06-01 15:56:26 -0400

    refactor: adding custom error handlers

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -13,6 +13,14 @@ func (app *application) routes() http.Handler {
 	// Initialize the router.
 	router := httprouter.New()
 
+	// Create a handler function which wraps our notFound() helper. and then
+	// assign it as the custom handler for 404 Not Found responses. You can also
+	// set a custom handler for 405 Method Not Allowed responses by setting
+	// router.NotFoundAllowed in the same way too.
+	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.notFound(w)
+	})
+
 	// Create a file server which serves files out of the "./ui/static" directory.
 	// Note that the path given to the http.Dir function is relative to the project
 	// directory root.
