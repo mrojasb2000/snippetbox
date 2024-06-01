ae8f4aedeebafde82b3c2351ec4132bfb9a2708e
Mauricio Rojas <mauricio.rojas@thoughtworks.com>
2024-06-01 15:27:49 -0400

    refactor: adding http router patterns

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,31 +1,33 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
 
 // The routes() method returns a servemux containing our application routes.
 // Update the signature for the routes() method so that it returns a
 // http.Handler instead of *http.ServerMux.
 func (app *application) routes() http.Handler {
-	// Swap the route declarations to use application struct's methods as the
-	// handler functions.
-	mux := http.NewServeMux()
+	// Initialize the router.
+	router := httprouter.New()
 
 	// Create a file server which serves files out of the "./ui/static" directory.
 	// Note that the path given to the http.Dir function is relative to the project
 	// directory root.
 	fileServer := http.FileServer(http.Dir("./ui/static"))
+	// Update the pattern for the route for the static files.
+	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
 
-	// Use the mux.Handle() function to register the file server as the handler for
-	// all URL paths that start with "/static/". For matching paths, we strip the
-	// "/static" prefix before the request reaches the file server.
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
-	// Register the other application routes as normal.
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
+	// And then create the routes using the appropiate methods, patterns and
+	// handlers.
+	router.HandlerFunc(http.MethodGet, "/", app.home)
+	router.HandlerFunc(http.MethodGet, "/snippet/view/:id", app.snippetView)
+	router.HandlerFunc(http.MethodGet, "/snippet/create", app.snippetCreate)
+	router.HandlerFunc(http.MethodPost, "/snippet/create", app.snippetCreatePost)
 
 	// Wrap the existing chain with the logRequest middleware
 	// Wrap the existing chain with the recoverPanic middleware.
-	return app.recoverPanic(app.logRequest(secureHeaders(mux)))
+	return app.recoverPanic(app.logRequest(secureHeaders(router)))
 }
