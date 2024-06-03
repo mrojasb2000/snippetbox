d35cd6ecba1487e9ff1206077864a6af879a37c9
Mauricio Rojas <mauricio.rojas@thoughtworks.com>
2024-06-03 18:13:17 -0400

    refactor: improve middleware chain with dynamic routes

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -42,9 +42,9 @@ func (app *application) routes() http.Handler {
 	// method returns a http.Handler (rather than a http.HandlerFunc) we also
 	// need to switch to registering to route using the router.Handler() method.
 	router.Handler(http.MethodGet, "/", dynamic.ThenFunc(app.home))
-	router.HandlerFunc(http.MethodGet, "/snippet/view/:id", app.snippetView)
-	router.HandlerFunc(http.MethodGet, "/snippet/create", app.snippetCreate)
-	router.HandlerFunc(http.MethodPost, "/snippet/create", app.snippetCreatePost)
+	router.Handler(http.MethodGet, "/snippet/view/:id", dynamic.ThenFunc(app.snippetView))
+	router.Handler(http.MethodGet, "/snippet/create", dynamic.ThenFunc(app.snippetCreate))
+	router.Handler(http.MethodPost, "/snippet/create", dynamic.ThenFunc(app.snippetCreatePost))
 
 	// Wrap the existing chain with the logRequest middleware
 	// Wrap the existing chain with the recoverPanic middleware.
